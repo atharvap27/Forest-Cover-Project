e2932e123df0474a93e2b5ef75086eca21a812cf
atharvap27 <71686966+atharvap27@users.noreply.github.com>
2021-07-03 19:22:36 +0530

    Add files via upload

---
+++ Training_DataTransformation/Train_DataTransformation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from app_logger.App_Logger import App_logger
+from os import listdir
+
+class DataTransformation():
+    def __init__(self):
+        self.logger=App_logger()
+        self.goodData_path='TrainingSet_Rawfiles_after_validation/GoodData/'
+
+    def addQuotestoSringValuesInColumn(self):
+        log_file = open('Training_logs/quotesToStringValues.txt','a+')
+        try:
+            files=[f for f in listdir(self.goodData_path)]
+            for file in files:
+                data=pd.read_csv(self.goodData_path+'/'+file)
+                data['class']=data['class'].apply(lambda x:"'"+str(x)+"'")
+                data.to_csv(self.goodData_path+file)
+                self.logger.log(log_file," %s: Quotes added successfully!!" % file)
+        except Exception as e:
+            self.logger.log(log_file,"Error in adding quotes to string values::%s"%e)
+            log_file.close()
+        log_file.close()
+
+
