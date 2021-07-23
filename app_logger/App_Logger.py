834974da3818ab882ece1174e35955a706d4b69a
atharvap27 <71686966+atharvap27@users.noreply.github.com>
2021-07-23 18:37:19 +0530

    Add files via upload

--- app_logger/App_Logger.py
+++ app_logger/App_Logger.py
@@ -4,10 +4,10 @@ class App_logger:
     def __init__(self):
         pass
 
-    def log(self,file_object,logging_message):
+    def log(self,file_object,log_message):
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        file_object.write(str(self.date)+"/"+str(self.current_time)+"\t\t"+ logging_message + '\n' )
+        file_object.write(str(self.date)+"/"+str(self.current_time)+"\t\t"+ log_message + '\n' )
 
 
